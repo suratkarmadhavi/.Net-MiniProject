6be12834702b876360db31e3ce8985fdc27e9e13
suratkarmadhavi <94161396+suratkarmadhavi@users.noreply.github.com>
2023-02-25 20:40:37 +0530

    Initial Commit

---
+++ EmployeeInfo/Migrations/20230225104101_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeInfo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tEmployee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tEmployee", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tEmployee");
+        }
+    }
+}
---
+++ EmployeeInfo/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeInfo.Models
+{
+    public class Employee
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+
+        public String City { get; set; }
+
+        public String State { get; set; }
+
+        public Decimal Salary { get; set; }
+    }
+}
